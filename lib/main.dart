ddb773e3a8a01a28514858d76bbac1869eb6c1cd
Jin Hang <jinhang@query.ai>
2020-01-26 06:45:15 -0600

    Add http package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:bonvallet/search/search.dart';
+import 'package:elastic_client/console_http_transport.dart';
 import 'package:flutter/material.dart';
+import 'package:elastic_client/elastic_client.dart' as elastic;
+
 
 void main() => runApp(MyApp());
 
@@ -25,19 +28,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
